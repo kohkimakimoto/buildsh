95a86c78084b45f6de668dae11c4ddfc56d1f940
Kohki Makimoto <kohki.makimoto@gmail.com>
2017-03-17 20:24:38 +0900

    up

--- examples/php-webserver.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-[ -z "$BUILDSH" ] && (sleep 1 && python -m webbrowser http://localhost:8888) &
-[ -z "$BUILDSH" ] && export BUILDSH_ADDITIONAL_DOCKER_OPTIONS="-i -t -p 8888:8888" && exec buildsh "$0" "$@"
-
-# start php builtin server
-php -S 0.0.0.0:8888
